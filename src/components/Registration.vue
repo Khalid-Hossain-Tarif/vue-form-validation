c7f2fb1eb08a0e1e6d8d4d59a9da36f2894c61fe
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-08-28 17:44:22 +0600

    refector code

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -4,10 +4,8 @@ import { ref, computed } from 'vue';
 const regex = {
     mobile: /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/,
     email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    password: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
-}
-
-const startValidation = ref(true);
+    password: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/,
+};
 
 const name = ref('');
 const number = ref('');
@@ -16,62 +14,82 @@ const password = ref('');
 const confirmPassword = ref('');
 
 const doRegistration = () => {
-    // startValidation.value = true;
-
-    if(isValidNumber.value == true && isValidEmail.value == true && isStrongPassword.value == true && isConfirmPassword.value == true) {
-        alert('Registering...')
+    if (
+        isValidNumber.value &&
+        isValidEmail.value &&
+        isStrongPassword.value &&
+        isConfirmPassword.value
+    ) {
+        alert('Registering...');
     }
-}
+};
 
 const isValidNumber = computed(() => {
-    return startValidation.value ? regex.mobile.test(number.value) : null;
+    return number.value ? regex.mobile.test(number.value) : null;
 });
 
 const isValidEmail = computed(() => {
-    return startValidation.value ? regex.email.test(email.value) : null;
+    return email.value ? regex.email.test(email.value) : null;
 });
 
 const isStrongPassword = computed(() => {
-    return startValidation.value ? regex.password.test(password.value) : null;
+    return password.value ? regex.password.test(password.value) : null;
 });
 
 const isConfirmPassword = computed(() => {
-    return startValidation.value ? password.value == confirmPassword.value : null;
+    return confirmPassword.value
+        ? password.value === confirmPassword.value
+        : null;
 });
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold text-blue-500 mb-10 text-center">Registration</h1>
+    <h1 class="text-3xl font-bold text-blue-500 mb-10 text-center">
+        Registration
+    </h1>
 
     <div class="bg-slate-100 rounded-xl p-10">
         <form action="">
             <div class="form-group">
                 <label class="primary-form-label" for="name">Name</label>
-                <input class="primary-input-field" type="text" name="" id="name" placeholder="Enter your name" v-model="name">
+                <input class="primary-input-field" type="text" name="" id="name" placeholder="Enter your name"
+                    v-model="name" />
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="number">Number</label>
-                <input class="primary-input-field" type="tel" name="" id="number" placeholder="Enter your number" v-model="number">
-                <div class="error-message" v-show="number !='' && !isValidNumber">Input a 11 digits number. Ex: 017******** / +880**********</div>
+                <input class="primary-input-field" type="tel" name="" id="number" placeholder="Enter your number"
+                    v-model="number" />
+                <div class="error-message" v-show="number !== '' && !isValidNumber">
+                    Input a 11 digits number. Ex: 017******** / +880**********
+                </div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="email">Email</label>
-                <input class="primary-input-field" type="email" name="" id="email" placeholder="Enter your email" v-model="email" required>
-                <div class="error-message" v-show="email !='' && !isValidEmail">Email didn't matched!</div>
+                <input class="primary-input-field" type="email" name="" id="email" placeholder="Enter your email"
+                    v-model="email" required />
+                <div class="error-message" v-show="email !== '' && !isValidEmail">
+                    Email didn't matched!
+                </div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="password">Password</label>
-                <input class="primary-input-field" type="password" name="" id="password" placeholder="Enter your password" v-model="password" required>
-                <div class="error-message" v-show="password !='' && !isStrongPassword">Weak Password</div>
+                <input class="primary-input-field" type="password" name="" id="password" placeholder="Enter your password"
+                    v-model="password" required />
+                <div class="error-message" v-show="password !== '' && !isStrongPassword">
+                    Weak Password
+                </div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="confirm-password">Confirm Password</label>
-                <input class="primary-input-field" type="password" name="" id="confirm-password" placeholder="Confirm your password" v-model="confirmPassword" required>
-                <div class="error-message" v-show="confirmPassword !='' && !isConfirmPassword">Password didn't match!</div>
+                <input class="primary-input-field" type="password" name="" id="confirm-password"
+                    placeholder="Confirm your password" v-model="confirmPassword" required />
+                <div class="error-message" v-show="confirmPassword !== '' && !isConfirmPassword">
+                    Password didn't match!
+                </div>
             </div>
 
             <div class="form-group">
