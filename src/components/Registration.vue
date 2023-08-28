9e9c9cacb483b1eabde0548d27a758238c3e6b16
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-08-28 16:03:59 +0600

    v-if replaced by v-show

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -53,25 +53,25 @@ const isConfirmPassword = computed(() => {
             <div class="form-group">
                 <label class="primary-form-label" for="number">Number</label>
                 <input class="primary-input-field" type="tel" name="" id="number" placeholder="Enter your number" v-model="number">
-                <div class="error-message" v-if="number !='' && !isValidNumber">Input a 11 digits number. Ex: 017******** / +880**********</div>
+                <div class="error-message" v-show="number !='' && !isValidNumber">Input a 11 digits number. Ex: 017******** / +880**********</div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="email">Email</label>
-                <input class="primary-input-field" type="email" name="" id="email" placeholder="Enter your email" v-model="email">
-                <div class="error-message" v-if="email !='' && !isValidEmail">Email didn't matched!</div>
+                <input class="primary-input-field" type="email" name="" id="email" placeholder="Enter your email" v-model="email" required>
+                <div class="error-message" v-show="email !='' && !isValidEmail">Email didn't matched!</div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="password">Password</label>
-                <input class="primary-input-field" type="password" name="" id="password" placeholder="Enter your password" v-model="password">
-                <div class="error-message" v-if="password !='' && !isStrongPassword">Weak Password</div>
+                <input class="primary-input-field" type="password" name="" id="password" placeholder="Enter your password" v-model="password" required>
+                <div class="error-message" v-show="password !='' && !isStrongPassword">Weak Password</div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="confirm-password">Confirm Password</label>
-                <input class="primary-input-field" type="password" name="" id="confirm-password" placeholder="Confirm your password" v-model="confirmPassword">
-                <div class="error-message" v-if="confirmPassword !='' && !isConfirmPassword">Password didn't match!</div>
+                <input class="primary-input-field" type="password" name="" id="confirm-password" placeholder="Confirm your password" v-model="confirmPassword" required>
+                <div class="error-message" v-show="confirmPassword !='' && !isConfirmPassword">Password didn't match!</div>
             </div>
 
             <div class="form-group">
