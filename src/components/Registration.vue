d74c7ba8ae0f2dabc54aabf523e1c27c38d3607b
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-08-28 13:02:19 +0600

    number and name field added & error message updated

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-// const name = ref('');
-// const number = ref('');
+const regex = {
+    mobile: /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/,
+    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    password: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/
+}
+
+const name = ref('');
+const number = ref('');
 const email = ref('');
 const password = ref('');
 const confirmPassword = ref('');
@@ -10,19 +16,23 @@ const confirmPassword = ref('');
 const startValidation = ref('');
 
 const doRegistration = () => {
-    startValidation.value = true;
+    // startValidation.value = true;
 
-    if(isValidEmail.value == true && isStrongPassword.value == true && isConfirmPassword.value == true) {
+    if(isValidNumber.value == true && isValidEmail.value == true && isStrongPassword.value == true && isConfirmPassword.value == true) {
         console.log('Registering...')
     }
 }
 
+const isValidNumber = computed(() => {
+    return startValidation.value ? regex.mobile.test(number.value) : null;
+});
+
 const isValidEmail = computed(() => {
-    return startValidation.value ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) : null;
+    return startValidation.value ? regex.email.test(email.value) : null;
 });
 
 const isStrongPassword = computed(() => {
-    return startValidation.value ? /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/.test(password.value) : null;
+    return startValidation.value ? regex.password.test(password.value) : null;
 });
 
 const isConfirmPassword = computed(() => {
@@ -33,37 +43,36 @@ const isConfirmPassword = computed(() => {
 <template>
     <h1 class="text-3xl font-bold text-blue-500 mb-10 text-center">Registration</h1>
 
+    {{ startValidation }}
     <div class="bg-slate-100 rounded-xl p-10">
         <form action="">
-            <!-- <div class="form-group">
+            <div class="form-group">
                 <label class="primary-form-label" for="name">Name</label>
                 <input class="primary-input-field" type="text" name="" id="name" placeholder="Enter your name" v-model="name">
-            </div> -->
+            </div>
 
-            <!-- <div class="form-group">
+            <div class="form-group">
                 <label class="primary-form-label" for="number">Number</label>
                 <input class="primary-input-field" type="tel" name="" id="number" placeholder="Enter your number" v-model="number">
-            </div> -->
+                <div class="error-message" v-if="number !='' && !isValidNumber">Input a 11 digits number. Ex: 017******** / +880**********</div>
+            </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="email">Email</label>
-                {{ isValidEmail }}
                 <input class="primary-input-field" type="email" name="" id="email" placeholder="Enter your email" v-model="email">
-                <div v-if="email !='' && !isValidEmail">Email didn't matched!</div>
+                <div class="error-message" v-if="email !='' && !isValidEmail">Email didn't matched!</div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="password">Password</label>
-                {{ isStrongPassword }} - {{ password }}
                 <input class="primary-input-field" type="password" name="" id="password" placeholder="Enter your password" v-model="password">
-                <div v-if="password !='' && !isStrongPassword">Weak Password</div>
+                <div class="error-message" v-if="password !='' && !isStrongPassword">Weak Password</div>
             </div>
 
             <div class="form-group">
                 <label class="primary-form-label" for="confirm-password">Confirm Password</label>
-                {{ isConfirmPassword }} - {{ confirmPassword }}
                 <input class="primary-input-field" type="password" name="" id="confirm-password" placeholder="Confirm your password" v-model="confirmPassword">
-                <!-- <div v-if="confirmPassword='' && !isConfirmPassword">Password didn't match!</div> -->
+                <div class="error-message" v-if="confirmPassword !='' && !isConfirmPassword">Password didn't match!</div>
             </div>
 
             <div class="form-group">
